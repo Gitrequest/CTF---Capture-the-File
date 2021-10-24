bd619cc84cb333b1fd85b72a756c825909260aad
Xiras <benny.lueffe@gmail.com>
2021-10-24 13:44:15 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace CTF___Capture_the_File
+{
+    partial class ManFrame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_back = new System.Windows.Forms.Button();
+            this.lbl_path = new System.Windows.Forms.Label();
+            this.txt_path = new System.Windows.Forms.TextBox();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.btn_forward = new System.Windows.Forms.Button();
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // btn_back
+            // 
+            this.btn_back.FlatAppearance.BorderSize = 0;
+            this.btn_back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_back.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_back.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(145)))), ((int)(((byte)(45)))));
+            this.btn_back.Location = new System.Drawing.Point(12, 12);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(54, 36);
+            this.btn_back.TabIndex = 0;
+            this.btn_back.Text = "<<";
+            this.btn_back.UseVisualStyleBackColor = true;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // lbl_path
+            // 
+            this.lbl_path.AutoSize = true;
+            this.lbl_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_path.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(45)))), ((int)(((byte)(86)))));
+            this.lbl_path.Location = new System.Drawing.Point(164, 18);
+            this.lbl_path.Name = "lbl_path";
+            this.lbl_path.Size = new System.Drawing.Size(57, 24);
+            this.lbl_path.TabIndex = 1;
+            this.lbl_path.Text = "Path:";
+            // 
+            // txt_path
+            // 
+            this.txt_path.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(54)))), ((int)(((byte)(50)))));
+            this.txt_path.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_path.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(45)))), ((int)(((byte)(86)))));
+            this.txt_path.Location = new System.Drawing.Point(227, 23);
+            this.txt_path.Name = "txt_path";
+            this.txt_path.ReadOnly = true;
+            this.txt_path.Size = new System.Drawing.Size(515, 15);
+            this.txt_path.TabIndex = 2;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.FlatAppearance.BorderSize = 0;
+            this.btn_exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(45)))), ((int)(((byte)(86)))));
+            this.btn_exit.Location = new System.Drawing.Point(941, 12);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(81, 36);
+            this.btn_exit.TabIndex = 3;
+            this.btn_exit.Text = "X";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // btn_open
+            // 
+            this.btn_open.FlatAppearance.BorderSize = 0;
+            this.btn_open.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_open.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_open.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(145)))), ((int)(((byte)(45)))));
+            this.btn_open.Location = new System.Drawing.Point(757, 11);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(76, 33);
+            this.btn_open.TabIndex = 4;
+            this.btn_open.Text = "Open";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // btn_forward
+            // 
+            this.btn_forward.FlatAppearance.BorderSize = 0;
+            this.btn_forward.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_forward.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_forward.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(145)))), ((int)(((byte)(45)))));
+            this.btn_forward.Location = new System.Drawing.Point(72, 12);
+            this.btn_forward.Name = "btn_forward";
+            this.btn_forward.Size = new System.Drawing.Size(56, 36);
+            this.btn_forward.TabIndex = 5;
+            this.btn_forward.Text = ">>";
+            this.btn_forward.UseVisualStyleBackColor = true;
+            this.btn_forward.Click += new System.EventHandler(this.btn_forward_Click);
+            // 
+            // browser
+            // 
+            this.browser.Location = new System.Drawing.Point(12, 54);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(1010, 615);
+            this.browser.TabIndex = 6;
+            // 
+            // ManFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(54)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(1034, 681);
+            this.Controls.Add(this.browser);
+            this.Controls.Add(this.btn_forward);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.txt_path);
+            this.Controls.Add(this.lbl_path);
+            this.Controls.Add(this.btn_back);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ManFrame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.Label lbl_path;
+        private System.Windows.Forms.TextBox txt_path;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.Button btn_forward;
+        private System.Windows.Forms.WebBrowser browser;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CTF___Capture_the_File
+{
+    public partial class ManFrame : Form
+    {
+        public ManFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            using(FolderBrowserDialog fbd = new FolderBrowserDialog() {Description="Select your path" })
+            {
+                if(fbd.ShowDialog()==DialogResult.OK)
+                {
+                    browser.Url = new Uri(fbd.SelectedPath);
+                    txt_path.Text = fbd.SelectedPath;
+                    //txt_path.ForeColor = System.Drawing.Color.FromArgb(18, 45, 86);
+
+                }
+                    
+                
+            }
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            if (browser.CanGoBack)
+                browser.GoBack();
+        }
+
+        private void btn_forward_Click(object sender, EventArgs e)
+        {
+            if (browser.CanGoForward)
+                browser.GoForward();
+        }
+    }
+}
